878191e7d14c4f6252ee1d4e40a9d04838c5cc5c
DESKTOP-SNQDOMD\pinoxchio <chayveng@outlook.com>
2024-05-20 12:22:20 +0700

    docker-compose rewrite

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ app = app_creator.app
 if __name__ == "__main__":
     print("Startup")
     # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8088)
